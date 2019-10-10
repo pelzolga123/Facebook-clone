caf10649cf0e28f2858eab9d6fae888b8f250264
pelzolga123 <pelzolga123@gmail.com>
2019-10-10 20:29:02 +0300

    Removed comments

--- fb-clone/app/models/post.rb
+++ fb-clone/app/models/post.rb
@@ -1,9 +1,7 @@
 class Post < ApplicationRecord
   #scope { order(created_at: :desc) }
   belongs_to :user
-<<<<<<< HEAD
 
-=======
   validates :content, presence: true, length: { minimum: 5, maximum: 500 }
->>>>>>> 8ea8bf8961361f4ea072dcb052f91be58e1b5588
+
 end
