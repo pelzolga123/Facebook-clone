fb91da2211224907daa508562ea15312012c460c
pelzolga123 <pelzolga123@gmail.com>
2019-10-11 18:43:36 +0300

    Fixed stickler errors

--- fb-clone/spec/features/log_in_spec.rb
+++ fb-clone/spec/features/log_in_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.feature 'LogIns', type: :feature do
   scenario 'valid inputs' do
-    User.create!(first_name: 'john', last_name: 'johnson', date_of_birth: '09/07/1967', gender: 'male', email: 'john@example.com', password: 'foobar')
+    User.create!(first_name: 'john',
+                 last_name: 'johnson',
+                 date_of_birth: '09/07/1967',
+                 gender: 'male',
+                 email: 'john@example.com',
+                 password: 'foobar')
     visit user_session_path
     fill_in 'Email', with: 'john@example.com'
     fill_in 'Password', with: 'foobar'
--- fb-clone/spec/features/sign_up_spec.rb
+++ fb-clone/spec/features/sign_up_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
 
 RSpec.feature 'SignUps', type: :feature do
-
 end
