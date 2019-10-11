9c2a2cefa2d99d929dc6e34061d0196ec94bcccc
tundeiness <tundeiness@yahoo.co.uk>
2019-10-11 16:20:50 +0100

    login test fix

--- fb-clone/spec/features/log_in_spec.rb
+++ fb-clone/spec/features/log_in_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.feature 'LogIns', type: :feature do
   scenario 'valid inputs' do
-    User.create!(email: 'john@example.com', password: 'foobar')
+    User.create!(first_name: 'john', last_name: 'johnson', date_of_birth: '09/07/1967', gender: 'male', email: 'john@example.com', password: 'foobar')
     visit user_session_path
     fill_in 'Email', with: 'john@example.com'
     fill_in 'Password', with: 'foobar'
-    click_on 'Log in'
+    find(:css, '.btn-primary').click
 
     expect(page).to have_content('Signed in successfully')
   end
--- fb-clone/spec/features/sign_up_spec.rb
+++ fb-clone/spec/features/sign_up_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
 
 RSpec.feature 'SignUps', type: :feature do
+
 end
