c0568a0f709311581c62452140d68fe8fbb5cdd8
tundeiness <tundeiness@yahoo.co.uk>
2019-10-14 17:35:43 +0100

    run db:migrate

--- fb-clone/db/schema.rb
+++ fb-clone/db/schema.rb
@@ -46,8 +46,6 @@ ActiveRecord::Schema.define(version: 2019_10_14_102158) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -56,7 +54,6 @@ ActiveRecord::Schema.define(version: 2019_10_14_102158) do
     t.datetime "date_of_birth"
     t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
