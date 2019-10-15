9d5da94159002d5ab20a0aed3ecc72149b4ea41c
tundeiness <tundeiness@yahoo.co.uk>
2019-10-15 12:26:20 +0100

    complete create post test when user logs in

---
+++ fb-clone/spec/features/create_post_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature 'CreatePosts', type: :feature do
+
+  before :each do
+    User.create(first_name: 'john',
+                last_name: 'johnson',
+                date_of_birth: '09/07/1967',
+                gender: 'male',
+                email: 'john@example.com',
+                password: 'foobar')
+  end
+
+  scenario 'authenticated user creates Post' do
+    visit user_session_path
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'foobar'
+    find(:css, '.btn-primary').click
+
+    expect(page).to have_content('Signed in successfully')
+    find('button', text: 'New Post').click_link('New Post')
+    fill_in 'Content', with: 'This is a test drive of the post as written by a user'
+    click_on 'Post'
+    expect(page).to have_content('Post was successfully created')
+    expect(page).to have_content('This is a test drive of the post as written by a user')
+  end
+
+end
