41a27bc5d68d9007cf585c858663d46fc90c3cf8
tundeiness <tundeiness@yahoo.co.uk>
2019-10-18 12:34:39 +0100

    complete validation test

--- fb-clone/spec/models/friendship_spec.rb
+++ fb-clone/spec/models/friendship_spec.rb
@@ -1,9 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
-  let(:user) { build(:user) }
-  let(:user_2) { build(:user) }
-  let(:friendship) { build(:friendship) }
 
+
+  context 'Validation tests' do
+    let(:user) { build(:user) }
+    let(:friendship) { build(:friendship) }
+
+    it 'ensures friendship is invalid without user and friend' do
+      friendship.user = nil
+      friendship.friend = nil
+      expect(friendship.save).to eq(false)
+    end
+
+    it 'ensures friendship is invalid with user and without friend' do
+      friendship.user = user
+      friendship.friend = nil
+      expect(friendship.save).to eq(false)
+    end
+
+    it 'ensures friendship is invalid without user but with friend' do
+      friendship.user = nil
+      friendship.friend = user
+      expect(friendship.save).to eq(false)
+    end
+  end
 
 end
