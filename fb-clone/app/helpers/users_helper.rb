023e2a1d75f6e01d1d1102fbd4fb57a61c8a7728
pelzolga123 <pelzolga123@gmail.com>
2019-10-18 20:09:22 +0300

    Fixed linter errors

--- fb-clone/app/helpers/users_helper.rb
+++ fb-clone/app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
-    def friendship(user)
-        Friendship.find_by(user_id: user.id, friend_id: current_user.id)
-    end
+  def friendship(user)
+    Friendship.find_by(user_id: user.id, friend_id: current_user.id)
+  end
 end
