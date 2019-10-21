e29cc665f84b3575dfa801f8c23b81e913ddbd16
tundeiness <tundeiness@yahoo.co.uk>
2019-10-21 16:26:28 +0100

    create authtest branch, setup Omniauth

--- fb-clone/db/schema.rb
+++ fb-clone/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_18_223223) do
+ActiveRecord::Schema.define(version: 2019_10_21_151942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,9 @@ ActiveRecord::Schema.define(version: 2019_10_18_223223) do
     t.string "last_name"
     t.datetime "date_of_birth"
     t.string "gender"
+    t.string "provider"
+    t.string "uid"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
