44295668d91bcbb3a97c5445583285e66fe639ac
tundeiness <tundeiness@yahoo.co.uk>
2019-10-22 10:56:41 +0100

    add omniauth controller

--- fb-clone/app/models/user.rb
+++ fb-clone/app/models/user.rb
@@ -14,4 +14,22 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :date_of_birth, presence: true
   validates :gender, presence: true
+
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.first_name
+      user.image = auth.info.avatar
+    end
+  end
 end
