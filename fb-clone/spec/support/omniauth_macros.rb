f7eecbc61b7c7c82c71ccee9cfd1fad78b7342ca
tundeiness <tundeiness@yahoo.co.uk>
2019-10-23 17:29:19 +0100

    add module

---
+++ fb-clone/spec/support/omniauth_macros.rb
@@ -0,0 +1,18 @@
+module OmniauthMacros
+  def mock_auth_hash
+    # The mock_auth configuration allows you to set per-provider (or default)
+    # authentication hashes to return during integration testing.
+    OmniAuth.config.mock_auth[:twitter] = {
+      'provider' => 'facebook',
+      'uid' => '123545',
+      'user_info' => {
+        'name' => 'mockuser',
+        'image' => 'mock_user_thumbnail_url'
+      },
+      'credentials' => {
+        'token' => 'mock_token',
+        'secret' => 'mock_secret'
+      }
+    }
+  end
+end
